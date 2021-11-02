6077fd0826f77a28a78684d1404d7f38c66dd692
Kieran Harris <kharris19063@gmail.com>
2021-11-01 23:38:30 -0400

    Installed inquirer, added user questions to array

--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const Choices = require("inquirer/lib/objects/choices");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Project title
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter the title of your project here."
+    },
+    // Project description
+    {
+        type: "input",
+        name: "description",
+        message: "Please add a detailed description of your project here."
+    },
+    // Project installation instructions
+    {
+        type: "input",
+        name: "installation",
+        message: "Please add instructions on how to install your project here."
+    },
+    // Project usage information
+    {
+        type: "input",
+        name: "usage",
+        message: "Please describe how to use your project."
+    },
+    // Project contribution guidelines
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please describe how users can contribution to your project"
+    },
+    // Project testing instructions
+    {
+        type: "input",
+        name: "testing",
+        message: "Please describe how users can test your project."
+    },
+    // Licensing Options - Thought it made more sense to use a checkbox in case user has multiple licenses required for a project
+    {
+        type: "checkbox",
+        name: "licensing",
+        message: "Please choose any required linceses for your project.",
+        choices: ["License 1", "License 2", "License 3", "License 4", "License 5"]
+    },
+    // Github UN
+    {
+        type: "input",
+        name: "githubUN",
+        message: "Please enter your Github username."
+    },
+    // Email contact
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address for user to contact with questions"
+    },
+];
+// The badge for that lincense is added to top of readme and a notice is added to that section of the readme titled License that explains which license the application is covered under
+// Github username that is added to the section of the readme entitled Questions with a link to users github profile
+// Email address is added to the section of the readme entitled questions with instructions on how to reach me with additional questions
+// Clicking on the links of the table of contents takes user to corresponding section of the readme
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) // Need to throw error if any
+            throw err;
+            console.log("Inputs added to README")
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+}
 
 // Function call to initialize app
 init();
